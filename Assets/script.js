93cadc3afaee574cc9e0ee01674456f98964888b
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-19 13:57:22 -0500

    Weather and sun cards work with JS now

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,34 @@
 $(document).ready(function () {
-  var APIKey = "889cd95742cb4d318b134906ce82bcb0";
-  var city = [];
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&units=imperial&appid=" +
-    APIKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    console.log(response.name);
-    console.log("Temperature: " + response.main.temp + "°F");
-    console.log("Wind Speed: " + response.wind.speed + " mph");
-
-    var sunrise = response.sys.sunrise;
-    var date = new Date(sunrise * 1000);
-    var time = date.toLocaleTimeString();
-    console.log(time);
-    console.log(time.split(":")[0] + ":" + time.split(":")[1] + " AM");
-
-    var sunset = response.sys.sunset;
-    var date = new Date(sunset * 1000);
-    var time2 = date.toLocaleTimeString();
-    console.log(time2);
-    console.log(time.split(":")[0] + ":" + time.split(":")[1] + " PM");
-  });
-
+var APIKey = "889cd95742cb4d318b134906ce82bcb0";
+    // var beachCityArray = ["Honolulu", "Miami", "Malibu", "Cancun", "Cape Cod"]
+    var beachCity = "Miami";
+    // var mountainCity = ["Breckenridge", "Park City Utah", "Mount Everest", "Mount Fuji", "Yosemeti"]
+    // var cityCity = ["London", "Paris", "Tokyo","New York", "Dubai"]
+    // var lakeCity = ["Lake Tahoe", "Lake Michigan", "Crater Lake", "Lake Powell", "Lake Superior"]
+    // var space = ["space"]
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + beachCity + "&units=imperial&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        $(".cityName").append(response.name);
+        $("#temp").append("Temp: " + response.main.temp + "°F");
+        $("#windSpeed").append("Wind: " + response.wind.speed + " mph");
+        var weatherIconEl = response.weather[0].icon;
+        var weatherIconUrl ="https://openweathermap.org/img/w/" + weatherIconEl + ".png";
+        $("#weatherIconUrl").attr("src", weatherIconUrl);
+        var sunrise = (response.sys.sunrise);
+        var date = new Date(sunrise * 1000);
+        var time = date.toLocaleTimeString();
+        $("#sunrise").append($("#sunriseTime").append(time.split(":")[0] + ":" + time.split(":")[1] + " AM"));
+        var sunset = (response.sys.sunset);
+        var date = new Date(sunset * 1000);
+        var time2 = date.toLocaleTimeString();
+        $("#sunset").append($("#sunsetTime").append(time2.split(":")[0] + ":" + time2.split(":")[1] + " PM"));
+        console.log(time2);
+        console.log(time2.split(":")[0] + ":" + time2.split(":")[1] + " PM");
+    })
   //Click on nav-bar, hides location.  I don't know if conditional works properly.
   $("#nav-bar").on("click", function (event) {
     event.preventDefault();
