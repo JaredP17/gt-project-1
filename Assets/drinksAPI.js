b7a3a52b8bc7e2eefc046363338c9b9ea9709659
Danielle Byrne <DanielleByrne421@gmail.com>
2020-06-19 13:02:58 -0400

    starting recipe click button

--- Assets/drinksAPI.js
+++ Assets/drinksAPI.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
   console.log("hi");
 
-
   var randomDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
   $.ajax({
     url: randomDrinkURL,
     method: "GET",
   }).then(function (randomDrinkResponse) {
-    console.log(randomDrinkResponse.drinks[0])
+    console.log(randomDrinkResponse.drinks[0]);
     $("#drink-name").text(randomDrinkResponse.drinks[0].strDrink);
-    var imgURL = randomDrinkResponse.drinks[0].strDrinkThumb;;
+    var imgURL = randomDrinkResponse.drinks[0].strDrinkThumb;
     $("#drink-pic").attr("src", imgURL);
+
+    $("#drink-recipe-button").on("click", function(event){
+      console.log(event);
+      //on click the button will expand with the revipe and the ingredients 
+    })
+
   });
 });
+ 
+
