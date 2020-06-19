7a5ed1632774fe449d3fdd49fa50eda65edf16fd
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-19 09:51:29 -0500

    Clicking on Nav Bar hides location container, and displays main-screen-container if hidden

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
 
-//Click on nav-bar, hides main screen.  This still needs a conditional. 
-$("#nav-bar").on("click", function(event){
-    event.preventDefault();
-    //if statement for "if main screen show, hide, if location screen, show"
-    $("#main-screen-container").hide();
-    console.log("nav-bar clicked");
-})
-
 
     var APIKey = "889cd95742cb4d318b134906ce82bcb0";
     var city = []
@@ -33,5 +25,18 @@ $("#nav-bar").on("click", function(event){
         console.log(time2);
         console.log(time.split(":")[0] + ":" + time.split(":")[1] + " PM");
     })
+
+
+    //Click on nav-bar, hides location.  I don't know if conditional works properly. 
+$("#nav-bar").on("click", function(event){
+    event.preventDefault();
+    //if statement for "if main screen show, hide, if location screen, show"
+   if($("#location-screen-container").css("display", "block")){
+    ($("#location-screen-container").css("display", "none"))
+   }
+   else{($(this).attr("display", "none"))}
+    console.log("nav-bar clicked");
+})
+
 
 })
